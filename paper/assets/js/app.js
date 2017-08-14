589def6e4a97fc83f40c2bb8e503788d9d00d2a3
신광원(개발인턴십) <codedraw@tmon.co.kr>
2017-08-14 10:10:12 +0900

    2017-08-14_10:09_신광원

--- paper/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('dealList', ['ngCookies']).config(function ($locationProvider, $httpProvider, $cookiesProvider) {
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
-	$httpProvider.defaults.withCredentials = true;
-	 $cookiesProvider.defaults.path = '/';
-  });
-
--- paper/assets/js/module.js
+++
@@ -1,6 +0,0 @@
-app.controller('printList', function($scope, $http) {
-    $http.get('http://127.0.0.1:8080/products').
-        then(function(response) {
-            $scope.List = response.data;
-        });
-});
